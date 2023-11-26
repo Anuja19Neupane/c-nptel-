74680a346b185346bee6d6763e4aafe20a1007ed
Anuja Neupane <anujaneupane20@gmail.com>
2023-11-26 08:26:24 +0545

    adding two numbers

---
+++ lec_2_io_loops/2_add_numbers.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main()
+{
+    int a, b;
+    std::cout << "Input two numbers: \n";
+    std::cin >> a >> b; // >> is output streaming operator
+    int sum = a + b;
+    std::cout << "Sum of" << a << "and" << b << "is: " << sum << std::endl;
+}
