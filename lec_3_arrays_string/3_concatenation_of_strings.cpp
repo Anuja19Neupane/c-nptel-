94c81e936724ea9ff5040a54b51a5ae23c2dd1d4
Anuja Neupane <anujaneupane20@gmail.com>
2023-12-01 08:18:45 +0545

    string concatenation

---
+++ lec_3_arrays_string/3_concatenation_of_strings.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str1 = "HELLO";
+    string str2 = "WPRLD";
+    string str = str1 + str2;
+
+    cout << str;
+}
