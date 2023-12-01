8da23e41fd76c96e4a06b835e25f3fb0122793ea
Anuja Neupane <anujaneupane20@gmail.com>
2023-12-01 08:58:50 +0545

    arrays

---
+++ lec_4_sorting _searching/4_replace_array_data.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm> // to use replace function
+using namespace std;
+
+int main()
+{
+    int data[] = {1, 2, 3, 4, 5};
+    replace(data, data + 5, 3, 2);
+    for (int i=0; i<5; ++i)
+    {
+        cout << data[i] << " ";
+    }
+    return 0;
+}
