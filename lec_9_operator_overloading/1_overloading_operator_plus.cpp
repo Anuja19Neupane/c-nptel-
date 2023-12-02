16c6e6af30a0f48188cd2731df40c5ff8901e1fd
Anuja Neupane <anujaneupane20@gmail.com>
2023-12-02 09:42:01 +0545

    overloading operator +

---
+++ lec_9_operator_overloading/1_overloading_operator_plus.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+enum E
+{
+    C0 = 0,
+    C1 = 1,
+    C2 = 2
+};
+E operator+(const E &a, const E &b) // overloaded operator +
+{
+    unsigned int uia = a, uib = b;
+    unsigned int t = (uia + uib) % 3;
+    return (E)t;
+}
+int main()
+{
+    E a = C1, b = C2;
+    int x = -1;
+    x=a+b; // overloaded operator + for enum E
+    cout << x << endl;
+}
